48390f0a93d5384c75fc6a113be1c3a7453124e6
Lapo0 <lapo.gini.01@gmail.com>
2023-04-04 16:53:46 +0200

    FEAT route services

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/contacts', function () {
 Route::get('/about', function () {
     return view('about');
 });
+
+Route::get('/services', function () {
+    return view('services');
+});
